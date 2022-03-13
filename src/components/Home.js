2c526870ac6183850fdbe49369bca161db01c8c0
ConnorPeng <pjh_jinghong@outlook.com>
2022-03-13 17:26:54 -0400

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
 function Home() {
-  return <div>Home</div>;
+  return <Container></Container>;
 }
+
+const Container = styled.div`
+  height: 100vh;
+`;
 
 export default Home;
