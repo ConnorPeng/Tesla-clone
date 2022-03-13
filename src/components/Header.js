c340b66faab39b4f2495f1e22b70cb9ca5721852
ConnorPeng <pjh_jinghong@outlook.com>
2022-03-13 19:34:56 -0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,52 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <p>
+          <a href="#">Model S</a>
+        </p>
+        <p>
+          <a href="#">Model Y</a>
+        </p>
+        <p>
+          <a href="#">Model 3</a>
+        </p>
+        <p>
+          <a href="#">Model X</a>
+        </p>
+      </Menu>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  p {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
 
 export default Header;
