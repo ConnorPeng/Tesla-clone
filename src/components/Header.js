cf278163b6cad128a34877a2607c3c672a09afec
ConnorPeng <pjh_jinghong@outlook.com>
2022-03-14 00:04:08 -0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 function Header() {
-  const [burgerStatus, setBurgerStatus] = useState(false);
+  const [burgerStatus, setBurgerStatus] = useState(true);
   return (
     <Container>
       <a>
@@ -102,7 +102,7 @@ const BurgerNav = styled.div`
   display: flex;
   flex-direction: column;
   text-align: start;
-  transform: {props => props.show ? translate(0) : translateX(100%)};
+  transform: ${(props) => (props.show ? "translateX(100%)" : "translateX(0)")};
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, 0.2);
